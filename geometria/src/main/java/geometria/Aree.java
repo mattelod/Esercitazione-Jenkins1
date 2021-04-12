4192e5e677b984106621cda355bb4df8f7c611be
Matteo <matte.lod98@gmail.com>
2021-04-12 16:31:50 +0200

    upload del progetto

---
+++ geometria/src/main/java/geometria/Aree.java
@@ -0,0 +1,38 @@
+package geometria;
+
+public class Aree {
+	
+
+	
+	public static int triangolo(int a, int b) {
+		
+		return (a*b)/2;
+		
+	}
+	
+	public static int quadrato(int a) {
+		
+		return a*a;
+		
+	}
+	
+	public static int rombo(int a, int b) {
+		
+		int A;
+		
+		A = a*b/2;
+		
+		return A;
+		
+		
+		
+	}
+	
+	public static int trapezio(int a, int b, int c) {
+		
+		return ((a+b)*c)/2;
+	}
+
+	
+
+}
---
+++ geometria/src/test/java/geometria/AreeTest.java
@@ -0,0 +1,45 @@
+package geometria;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AreeTest {
+
+	@Test
+	public void testTriangolo() {
+		int a = 5;
+		int b = 2;
+		int valoreAtteso = 5;
+		int valoreCalcolato = Aree.triangolo(a, b);
+		assertEquals(valoreAtteso, valoreCalcolato);
+
+	}
+
+	@Test
+	public void testQuadrato() {
+		int a = 10;
+		int valoreAtteso = 100;
+		int valoreCalcolato = Aree.quadrato(a);
+		assertEquals(valoreAtteso, valoreCalcolato);
+	}
+
+	@Test
+	public void testRombo() {
+		int a = 6;
+		int b = 5;
+		int valoreAtteso = 15;
+		int valoreCalcolato = Aree.rombo(a, b);
+		assertEquals(valoreAtteso, valoreCalcolato);
+	}
+	
+	public void testTrapezio() {
+		int a = 2;
+		int b = 3;
+		int c = 4;
+		int valoreAtteso = 10;
+		int valoreCalcolato = Aree.trapezio(a, b, c);
+		assertEquals(valoreAtteso, valoreCalcolato);
+	}
+
+}
